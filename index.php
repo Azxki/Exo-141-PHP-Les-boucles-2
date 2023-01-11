3c692383b0cf14eabbe26be2da9fb8a00d300bd3
azxki <cococoursan@gmail.com>
2023-01-11 16:41:43 +0100

    exo

---
+++ index.php
@@ -0,0 +1,105 @@
+//EXO 1 <br> <br>
+<?php
+$maVar = 0;
+
+while ($maVar < 10) {
+    echo "Ma variable vaut " . $maVar;
+    $maVar++;
+}
+?>
+
+<br> <br>
+//EXO 2 <br> <br>
+<?php
+$maVar1 = 0;
+$maVar2 = 5;
+
+while ($maVar1 < 20) {
+    echo "maVar vaut " . $maVar1 * $maVar2;
+    $maVar1++;
+}
+?>
+<br> <br>
+//EXO 3 <br> <br>
+<?php
+$truc = 100;
+$bidule = 1;
+
+//while ($truc = 10|| $truc < 10) {
+//    echo "hello";
+//}
+?>
+<br> <br>
+//EXO 4 <br> <br>
+<?php
+$oui = 1;
+
+while ($oui !== 10) {
+    echo "ma var : " . $oui/2;
+    $oui++;
+}
+?>
+<br> <br>
+//EXO 5 <br> <br>
+<?php
+$t = 1;
+
+while ($t < 15) {
+    echo "Ony est presque";
+    $t++;
+}
+?>
+<br> <br>
+//EXO 6 <br> <br>
+<?php
+
+?>
+<br> <br>
+//EXO 7 <br> <br>
+<?php
+$y = 1;
+
+while ($y < 100) {
+    echo "On y est presque";
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+    $y++;
+}
+?>
+<br> <br>
+//EXO 8 <br> <br>
+<?php
+$x = 200;
+
+while ($x > 0) {
+    echo "Enfin !!!";
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+    $x--;
+}
+?>
+<br> <br>
+
+
+
